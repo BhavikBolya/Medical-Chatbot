078d919d32c63415c50d96128856bad7ae9d5c8b
Bhavik Bolya <bhavikbolya2911@gmail.com>
2021-03-04 01:14:01 +0530

    we are god

--- Bhavik_testing.py
+++ Bhavik_testing.py
@@ -1,16 +1,30 @@
-import nltk
-from nltk.corpus import stopwords
-from nltk import word_tokenize
+# import nltk
+# from nltk.corpus import stopwords
+# from nltk import word_tokenize
 
-word1 = input()
-word = word_tokenize(word1)
-print(word)
-stopwords = stopwords.words("english")
-clean = []
-for w in word:
-    if w not in stopwords:
-        clean.append(w)
+# word1 = input()
+# word = word_tokenize(word1)
+# print(word)
+# stopwords = stopwords.words("english")
+# clean = []
+# for w in word:
+#     if w not in stopwords:
+#         clean.append(w)
 
-print(clean)
-print("\n")
-print(len(clean))
+# print(clean)
+# print("\n")
+# print(len(clean))
+
+
+import pandas as pd
+import numpy as np
+
+df =  pd.read_csv('Data\Training.csv')
+
+l1 = ['prominent_veins_on_calf']
+
+for col in df.columns:
+    for i in l1:
+        if col==i:
+            df = df.loc[df[col]==1]
+print(df['prognosis'])
