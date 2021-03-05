c502008190115213e049760dab44b0a9a3dd91b1
Bhavik Bolya <bhavikbolya2911@gmail.com>
2021-03-05 18:27:28 +0530

    Apriori algo

--- Bhavik_testing.py
+++ Bhavik_testing.py
@@ -16,11 +16,48 @@
 # print(len(clean))
 
 
-import pandas as pd
-df =  pd.read_csv('Data\Training.csv')
-l1 = ['extra_marital_contacts']
-for col in df.columns:
-    for i in l1:
-        if col==i:
-            df = df.loc[df[col]==1]
-print(df['prognosis'])
+# import pandas as pd
+# df =  pd.read_csv('Data\Training.csv')
+# l1 = ['pain_in_anal_region']
+# for col in df.columns:
+#     for i in l1:
+#         if col==i:
+#             df = df.loc[df[col]==1]
+# print(df['prognosis'])
+
+# Program to measure the similarity between 
+# two sentences using cosine similarity. 
+from nltk.corpus import stopwords 
+from nltk.tokenize import word_tokenize 
+
+# X = input("Enter first string: ").lower() 
+# Y = input("Enter second string: ").lower() 
+X ="fever"
+Y ="high fever"
+
+# tokenization 
+X_list = word_tokenize(X) 
+Y_list = word_tokenize(Y) 
+
+# sw contains the list of stopwords 
+sw = stopwords.words('english') 
+l1 =[];l2 =[] 
+
+# remove stop words from the string 
+X_set = {w for w in X_list if not w in sw} 
+Y_set = {w for w in Y_list if not w in sw} 
+
+# form a set containing keywords of both strings 
+rvector = X_set.union(Y_set) 
+for w in rvector: 
+	if w in X_set: l1.append(1) # create a vector 
+	else: l1.append(0) 
+	if w in Y_set: l2.append(1) 
+	else: l2.append(0) 
+c = 0
+
+# cosine formula 
+for i in range(len(rvector)): 
+		c+= l1[i]*l2[i] 
+cosine = c / float((sum(l1)*sum(l2))**0.5) 
+print("similarity: ", cosine) 
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Medical-Chatbot
 Bhavik Bolya
+# important links
+https://ijiet.com/
+http://ijiet.com/wp-content/uploads/2018/06/3.pdf
+https://analyticsindiamag.com/beginners-guide-to-understanding-apriori-algorithm-with-implementation-in-python/
+https://www.geeksforgeeks.org/python-measure-similarity-between-two-sentences-using-cosine-similarity/
