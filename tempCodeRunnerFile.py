be57ffb4915c888b0280781864af244f4747998d
Bhavik Bolya <bhavikbolya2911@gmail.com>
2021-04-12 00:27:28 +0530

    f

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,7 +1,7 @@
 import pandas as pd
 df =  pd.read_csv('Data\Training.csv')
 l1 = [1,0,0,0,1,1,1,0]
-for col in df.columns:
+for row in range(len(df)):
     for i in l1:
         if col==i:
             df = df.loc[df[col]==1]
