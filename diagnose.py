81b6b8cd0d7a6441eb0324a34c7c116a687eaf1a
HrishilMaliwal <72825538+HrishilMaliwal@users.noreply.github.com>
2021-04-12 22:44:01 +0530

    sa

--- diagnose.py
+++ diagnose.py
@@ -5,8 +5,8 @@ import spacy
 from sklearn.metrics.pairwise import cosine_similarity
 
 nlp = spacy.load('en_core_web_md')
-diagnosis_df = pd.read_pickle("input_data/diagnosis_data.pkl")
-symptoms_df = pd.read_pickle("input_data/symptoms.pkl")
+diagnosis_df = pd.read_pickle("diagnosis_data.pkl")
+symptoms_df = pd.read_pickle("symptoms.pkl")
 
 # logging config
 for handler in logging.root.handlers[:]:
